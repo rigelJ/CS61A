2fbf6c64da968c9f3556871bccc8f6a80167ee23
Donnie <49977256+rigelJ@users.noreply.github.com>
2021-05-28 20:03:10 +0800

    Delete Labs/lab00 directory

--- Labs/lab00/lab00.py
+++
@@ -1,9 +0,0 @@
-def twenty_nineteen():
-    """Come up with the most creative expression that evaluates to 2019,
-    using only numbers and the +, *, and - operators.
-
-    >>> twenty_nineteen()
-    2019
-    """
-    
-    return (2*1000) + 20 -1
