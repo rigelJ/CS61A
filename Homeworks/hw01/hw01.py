008a96a55cd4c4f53ec7000967f77e1048cc9563
donnie <rigelhunting@gmail.com>
2021-05-30 23:42:47 +0800

    +lab02

---
+++ Homeworks/hw01/hw01.py
@@ -0,0 +1,138 @@
+""" Homework 1: Control """
+
+from operator import add, sub
+from math import floor
+
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    """
+    if b < 0:
+        f = lambda a,b:a+(-b)
+    else:
+        f = lambda a,b:a+b
+    return f(a, b)
+
+def two_of_three(a, b, c):
+    """Return x*x + y*y, where x and y are the two largest members of the
+    positive numbers a, b, and c.
+
+    >>> two_of_three(1, 2, 3)
+    13
+    >>> two_of_three(5, 3, 1)
+    34
+    >>> two_of_three(10, 2, 8)
+    164
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    return (max(a,b,c))**2 + min(max(a,b),max(b,c),max(c,a))**2
+def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    sums = []
+    for i in range(n):
+        if i == 0:
+            continue
+        if n % i == 0:
+            sums.append(i)
+        else:
+            continue
+    s = max(sums)
+    return s
+            
+
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and
+    false_result otherwise.
+
+    >>> if_function(True, 2, 3)
+    2
+    >>> if_function(False, 2, 3)
+    3
+    >>> if_function(3==2, 3+2, 3-2)
+    1
+    >>> if_function(3>2, 3+2, 3-2)
+    5
+    """
+    if condition:
+        return true_result
+    else:
+        return false_result
+
+
+def with_if_statement():
+    """
+    >>> result = with_if_statement()
+    2
+    >>> print(result)
+    None
+    """
+    if c():
+        return t()
+    else:
+        return f()
+
+def with_if_function():
+    """
+    >>> result = with_if_function()
+    1
+    2
+    >>> print(result)
+    None
+    """
+    return if_function(c(), t(), f())
+
+def c():
+    return False
+
+def t():
+    print(1)
+    return None
+
+def f():
+    print(2)
+    return None
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    print(floor(n))
+    step = 1
+    while n!=1:
+        if n%2 == 0:
+            n=floor(n/2)
+            print(n)
+            step += 1
+        else:
+            n = floor(n*3+1)
+            print(n)
+            step += 1
+    return step
+
+
+
+
